09d90870377f895d3bb0194bf409962398a745fe
Raphael Catolino <raphael.catolino@gmail.com>
2017-01-01 19:34:21 +0100

    Fix method signatures for the pam_sm interface

--- src/pam.rs
+++ src/pam.rs
@@ -42,12 +42,12 @@ impl Pam {
 /// You should override functions depending on what kind of module you implement
 pub trait PamServiceModule {
     fn open_session(self: &Self, _: Pam, _: PamFlag,
-                        _: size_t, _: *const u8) -> PamReturnCode {
+                        _: size_t, _: *const *const u8) -> PamReturnCode {
         PamReturnCode::SERVICE_ERR
     }
 
     fn close_session(self: &Self, _: Pam, _: PamFlag,
-                         _: size_t, _: *const u8) -> PamReturnCode {
+                         _: size_t, _: *const *const u8) -> PamReturnCode {
         PamReturnCode::SERVICE_ERR
     }
 
@@ -57,17 +57,17 @@ pub trait PamServiceModule {
     }
 
     fn setcred(self: &Self, _: Pam, _: PamFlag,
-                   _: size_t, _: *const u8) -> PamReturnCode {
+                   _: size_t, _: *const *const u8) -> PamReturnCode {
         PamReturnCode::SERVICE_ERR
     }
 
     fn acct_mgmt(self: &Self, _: Pam, _: PamFlag,
-                     _: size_t, _: *const u8) -> PamReturnCode {
+                     _: size_t, _: *const *const u8) -> PamReturnCode {
         PamReturnCode::SERVICE_ERR
     }
 
     fn chauthtok(self: &Self, _: Pam, _: PamFlag,
-                     _: size_t, _: *const u8) -> PamReturnCode {
+                     _: size_t, _: *const *const u8) -> PamReturnCode {
         PamReturnCode::SERVICE_ERR
     }
 }
@@ -87,13 +87,13 @@ thread_local! {
 // Pam Callbacks
 #[no_mangle]
 pub extern "C" fn pam_sm_open_session(pamh: Pam, flags: PamFlag,
-                           argc: size_t, argv: *const u8) -> PamReturnCode {
+                           argc: size_t, argv: *const *const u8) -> PamReturnCode {
     return PAMSM.with(|sm| sm.open_session(pamh, flags, argc, argv));
 }
 
 #[no_mangle]
 pub extern "C" fn pam_sm_close_session(pamh: Pam, flags: PamFlag,
-                            argc: size_t, argv: *const u8) -> PamReturnCode {
+                            argc: size_t, argv: *const *const u8) -> PamReturnCode {
     return PAMSM.with(|sm| sm.close_session(pamh, flags, argc, argv));
 }
 
@@ -105,19 +105,19 @@ pub extern "C" fn pam_sm_authenticate(pamh: Pam, flags: PamFlag,
 
 #[no_mangle]
 pub extern "C" fn pam_sm_setcred(pamh: Pam, flags: PamFlag,
-                      argc: size_t, argv: *const u8) -> PamReturnCode {
+                      argc: size_t, argv: *const *const u8) -> PamReturnCode {
     return PAMSM.with(|sm| sm.setcred(pamh, flags, argc, argv));
 }
 
 #[no_mangle]
 pub extern "C" fn pam_sm_acct_mgmt(pamh: Pam, flags: PamFlag,
-                        argc: size_t, argv: *const u8) -> PamReturnCode {
+                        argc: size_t, argv: *const *const u8) -> PamReturnCode {
     return PAMSM.with(|sm| sm.acct_mgmt(pamh, flags, argc, argv));
 }
 
 #[no_mangle]
 pub extern "C" fn pam_sm_chauthtok(pamh: Pam, flags: PamFlag,
-                        argc: size_t, argv: *const u8) -> PamReturnCode {
+                        argc: size_t, argv: *const *const u8) -> PamReturnCode {
     return PAMSM.with(|sm| sm.chauthtok(pamh, flags, argc, argv));
 }
 
