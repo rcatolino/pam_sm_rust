5f8a3293f34855e69fb395bcb5353554387e8853
Miquel van Smoorenburg <mikevs@xs4all.net>
2018-08-04 17:00:45 +0200

    update example module

--- test-module/src/lib.rs
+++ test-module/src/lib.rs
@@ -1,16 +1,31 @@
+#[macro_use]
 extern crate pamsm;
 extern crate time;
 
-use pamsm::{PamServiceModule, Pam};
-use pamsm::pam_raw::{PamFlag, PamError};
+use pamsm::{PamServiceModule, Pam, PamFlag, PamError};
 
 struct PamTime;
 
 impl PamServiceModule for PamTime {
     fn authenticate(self: &Self, pamh: Pam, _: PamFlag, args: Vec<String>) -> PamError {
+
+        // If you need login/password here, that works like this:
+        //
+        //  let user = match pamh.get_user(None) {
+        //      Ok(Some(u)) => u,
+        //      Ok(None) => return PamError::USER_UNKNOWN,
+        //      Err(e) => return e,
+        //  };
+		//
+        //  let pass = match pamh.get_authtok(None) {
+        //      Ok(Some(p)) => p,
+        //      Ok(None) => return PamError::AUTH_ERR,
+        //      Err(e) => return e,
+        //  };
+
+        // Only allow authentication when it's 4 AM
         let hour = time::now().tm_hour;
         if hour != 4 {
-            // Only allow authentication when it's 4 AM
             PamError::SUCCESS
         } else {
             PamError::AUTH_ERR
@@ -18,10 +33,5 @@ impl PamServiceModule for PamTime {
     }
 }
 
-#[no_mangle]
-pub extern "C" fn get_pam_sm() -> Box<PamServiceModule> {
-    return Box::new(PamTime {});
-}
+pamsm_init!(Box::new(PamTime));
 
-pub fn main() {
-}
