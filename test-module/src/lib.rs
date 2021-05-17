b2274e8392e6507b93a93fdf8a99a78c2c5ff410
rca <raphael.catolino@gmail.com>
2021-05-17 19:51:52 +0100

    Try set_data/cleanup;

--- test-module/src/lib.rs
+++ test-module/src/lib.rs
@@ -2,37 +2,43 @@
 extern crate pamsm;
 extern crate time;
 
-use std::ops::Deref;
-use pamsm::{PamServiceModule, PamData, PamLibExt, Pam, PamFlag, PamError};
+use pamsm::{Pam, PamData, PamError, PamFlag, PamLibExt, PamServiceModule};
 
 struct PamTime;
 
 struct DateTime(time::OffsetDateTime);
+struct Test {
+    status: bool,
+    hour: u8,
+}
 
 impl PamData for DateTime {
     fn cleanup(&mut self, _pam: Pam, flags: i32, status: PamError) {
-        if (flags & PamFlag::PAM_SILENT as i32) != 0 {
-            println!("PamTime cleanup. Last authentication at {}, result {}", self.0, status);
+        if (flags & PamFlag::PAM_SILENT as i32) == 0 {
+            println!(
+                "PamTime cleanup date time. Last authentication at {}, result {}, flags {}",
+                self.0, status, flags
+            );
+            if (flags & PamFlag::PAM_DATA_REPLACE as i32) != 0 {
+                println!("Pam data is being replaced");
+            }
         }
     }
 }
 
-impl DateTime {
-    fn now() -> Self {
-        DateTime(time::OffsetDateTime::try_now_local().unwrap_or(time::OffsetDateTime::now_utc()))
-    }
-}
-
-impl Deref for DateTime {
-    type Target = time::OffsetDateTime;
-    fn deref(&self) -> &Self::Target {
-        &self.0
+impl PamData for Test {
+    fn cleanup(&mut self, _pam: Pam, flags: i32, status: PamError) {
+        if (flags & PamFlag::PAM_SILENT as i32) == 0 {
+            println!(
+                "PamTime cleanup test. Last authentication at {}h : {}, result {}, flags {}",
+                self.hour, self.status, status, flags
+            );
+        }
     }
 }
 
 impl PamServiceModule for PamTime {
     fn authenticate(pamh: Pam, _flags: PamFlag, _args: Vec<String>) -> PamError {
-
         // If you need password here, that works like this:
         //
         //  let pass = match pamh.get_authtok(None) {
@@ -41,21 +47,32 @@ impl PamServiceModule for PamTime {
         //      Err(e) => return e,
         //  };
 
-        // Only allow authentication when it's 4 AM and user name is root
-        let now = DateTime::now();
+        // Only allow authentication when it's not 4 AM and user name is root
+        let now = DateTime(time::OffsetDateTime::now_utc());
         let user = match pamh.get_user(None) {
             Ok(Some(u)) => u,
             Ok(None) => return PamError::USER_UNKNOWN,
             Err(e) => return e,
         };
 
-        let hour = now.hour();
+        let hour = now.0.hour();
         match pamh.set_data("pamtime", Box::new(now)) {
             Err(e) => return e,
             Ok(_) => (),
         };
 
-        if hour != 4 && user.to_str().unwrap_or("") == "root" {
+        let test = Test {
+            status: hour != 4 && user.to_str().unwrap_or("") == "root",
+            hour: hour,
+        };
+
+        let status = test.status;
+        match pamh.set_data("pamtime", Box::new(test)) {
+            Err(e) => return e,
+            Ok(_) => (),
+        };
+
+        if status {
             PamError::SUCCESS
         } else {
             PamError::AUTH_ERR
